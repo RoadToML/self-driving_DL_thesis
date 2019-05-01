ab1438f6fdcadcc2097e4dcac96e63997fcc2ddb
simran <simransingh9503@gmail.com>
2019-05-01 19:34:02 +1200

    added vehicle spawning

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from carla import Client
+from carla import Client, Transform, Rotation, Location
 
 import carla
 import random
+import time
 
 # starting proper work now
 
@@ -13,4 +14,19 @@ client.load_world('/Game/Carla/ExportedMaps/scenario_1_only_bends')
 print('connected!') # DEBUG
 
 world = client.get_world()
-print(client.get_available_maps())
+
+blueprint_lib = world.get_blueprint_library()
+vehicle_bp = random.choice(blueprint_lib.filter('vehicle.audi.*'))
+
+spawn_points = world.get_map().get_spawn_points()
+point = spawn_points[0]
+print(point)
+
+
+print('spawning car') # DEBUG
+
+# adding spawn point for car as per coordinates on unreal engine
+car_spawn_point = Transform(Location(x=-391.470120, y=-2.151211, z=7.446297))
+
+vehicle_actor = world.spawn_actor(vehicle_bp, point)
+print('DONE') # DEBUG
