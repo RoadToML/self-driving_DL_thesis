5e6cb19923c6883fc06096f4f10245f082a4a588
simran <simransingh9503@gmail.com>
2019-05-06 18:13:51 +1200

    test commit

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ print('attempting to connect...') # DEBUG
 client = Client('localhost', 2000, worker_threads = 12)
 client.set_timeout(10.0)
 
-client.load_world('/Game/Carla/ExportedMaps/only_bends_v2')
+client.load_world('/Game/Carla/Maps/single_left_bend')
 print('connected!') # DEBUG
 
 world = client.get_world()
@@ -18,15 +18,13 @@ world = client.get_world()
 blueprint_lib = world.get_blueprint_library()
 vehicle_bp = random.choice(blueprint_lib.filter('vehicle.audi.*'))
 
+# adding spawn point for car as per coordinates on unreal engine
 spawn_points = world.get_map().get_spawn_points()
 point = spawn_points[0]
-print(point)
+print(point) # DEBUG 
 
 
 print('spawning car') # DEBUG
-
-# adding spawn point for car as per coordinates on unreal engine
-car_spawn_point = Transform(Location(x=-391.470120, y=-2.151211, z=7.446297))
 
 vehicle_actor = world.spawn_actor(vehicle_bp, point)
 print('DONE') # DEBUG
