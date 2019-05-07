9946157fba73bc98b9ff6fd1e0976814fa73f954
simran <simransingh9503@gmail.com>
2019-05-07 20:51:38 +1200

    added turning functionality

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ blueprint_lib = world.get_blueprint_library()
 vehicle_bp = random.choice(blueprint_lib.filter('vehicle.audi.*'))
 
 # adding spawn point for car as per coordinates on unreal engine
-spawn_points = world.get_map().get_spawn_points()
+spawn_points = world.get_map().get_spawn_points() 
 point = spawn_points[0]
 print(point) # DEBUG 
 
@@ -30,4 +30,22 @@ vehicle_actor = world.spawn_actor(vehicle_bp, point)
 print('DONE') # DEBUG
 
 time.sleep(2)
-vehicle_actor.apply_control(carla.VehicleControl(throttle=1.0, steer=-1.0))
+# vehicle_actor.apply_control(carla.VehicleControl(throttle=1.0, steer=-1.0))
+
+location = vehicle_actor.get_location()
+transform = vehicle_actor.get_transform()
+print(transform)
+
+# ##############################################################
+# ##########this code allows for turning the car ###############
+# ##############################################################
+# while True:
+#     transform = vehicle_actor.get_transform()
+#     print(transform)
+#     transform.rotation.yaw += 3.0
+#     vehicle_actor.set_transform(transform)
+#     time.sleep(0.5)
+# ###############################################################
+
+# location.y = 3.0
+# actor.set_location(location)
