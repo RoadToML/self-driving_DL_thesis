24561ce9ed08edd269828105d714ddeb41e38a8d
simran Singh <simransingh9503@gmail.com>
2019-06-12 17:54:55 +1200

    complete change, function API used

--- model.py
+++ model.py
@@ -1,20 +1,23 @@
-from keras.models import Sequential
+from keras.models import Model
 from keras.layers import Conv2D, MaxPooling2D, Dense, Activation, Flatten
-from keras.layers import LeakyReLU
+from keras.layers import LeakyReLU, Concatenate, Input
 from keras import optimizers
 
 import keras
-import pandas as pd 
+import pandas as pd
 import numpy as np
 import cv2
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-import os 
+import os
 import time
 
 df = pd.read_csv('velocity_labels.csv',\
 names = ['image', 'velocity', 'steering_angle', 'outcome'],\
-converters = {'image': lambda x: str(x), 'outcome': lambda x: '1' if x.strip() == 'good' else '0'})
+converters = {'image': lambda x: str(x), 'outcome': lambda x: '1' if x.strip() == 'good' else '0',\
+                'steering_angle': lambda x: float(x)/70})
 
 print (df.tail()) # DEBUG
 
@@ -22,69 +25,83 @@ print (df.tail()) # DEBUG
 # Create training data labels and target
 
 training_data = []
-for img, label in zip(df['image'], df['steering_angle']):
-    image = cv2.imread(f'output/{img}.png', 1)
+for img, angle, outcome, velocity in zip(df['image'], df['steering_angle'], df['outcome'], df['velocity']):
+    image = cv2.imread(f"output//{img}.png", 1)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
     # plt.imshow(image)
-    # plt.show() 
-    training_data.append([image, label])
+    # plt.show()
+    if outcome == '1':
+        training_data.append([image, angle, velocity])
 
-X = [img for img, label in training_data]
-Y = [label for img, label in training_data]
+X = [img for img, label, velocity in training_data]
+Y = [label for img, label, velocity in training_data]
+other_inp = [velocity for img, label, velocity in training_data]
 
-X = np.array(X)
+X = np.array(X).reshape(-1, 940,940,3)
 Y = np.array(Y)
+other_inp = np.array(other_inp)
+print(other_inp.shape)
 
 
-# Create Model 
-model = Sequential()
+# Create Model
+def create_model(X, Y, other_inp):
+    # model = Sequential()
 
-# model.add(Flatten(input_shape = (940,940,3)))
-model.add(Conv2D(5, (5, 5), strides=(2, 2), input_shape = (940,940,3)))
-model.add(LeakyReLU())
-model.add(MaxPooling2D(pool_size=(2, 2)))
+    input1 = Input(shape=(940,940,3))
 
-model.add(Conv2D(5, (5, 5), strides=(2, 2)))
-model.add(LeakyReLU())
-model.add(MaxPooling2D(pool_size=(2, 2)))
+    # model.add(Flatten(input_shape = (940,940,3)))
+    conv1 = Conv2D(20, (5, 5), strides=(2, 2))(input1)
+    #conv1 = model.add(Conv2D(20, (5, 5), strides=(2, 2), input_shape = (940,940,3)))
+    activ1 =LeakyReLU()(conv1)
+    pool1 =MaxPooling2D(pool_size=(2, 2))(activ1)
 
-model.add(Conv2D(5, (5, 5), strides=(2, 2)))
-model.add(LeakyReLU())
-model.add(MaxPooling2D(pool_size=(2, 2)))
+    conv2 = Conv2D(16, (5, 5), strides=(2, 2))(pool1)
+    activ2 =LeakyReLU()(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(activ2)
 
-model.add(Conv2D(5, (3, 3), strides=1))
-model.add(LeakyReLU())
-model.add(MaxPooling2D(pool_size=(2, 2)))
+    conv3 = Conv2D(12, (5, 5), strides=(2, 2))(pool2)
+    activ3 = LeakyReLU()(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(activ3)
 
-model.add(Conv2D(5, (3, 3), strides=1))
-model.add(LeakyReLU())
-model.add(MaxPooling2D(pool_size=(2, 2)))
+    # z = model.add(Conv2D(8, (3, 3), strides=1))
+    # z = model.add(LeakyReLU())
+    # z = model.add(MaxPooling2D(pool_size=(2, 2)))
+    #
+    # z = model.add(Conv2D(4, (3, 3), strides=1))
+    # z = model.add(LeakyReLU())
+    # z = model.add(MaxPooling2D(pool_size=(2, 2)))
+    #
+    flat1 = Flatten()(pool3)
 
-model.add(Flatten())
-model.add(Dense(1164)) 
-model.add(Dense(100))
-model.add(Dense(50))
-model.add(Dense(10))
-model.add(Activation('tanh'))
+    input2 = Input(shape = (1, ))
+    concat1 = Concatenate(axis=1)([flat1, input2])
+    # z = model.add(Dense(1164))
+    # z = model.add(Dense(100))
+    # z = model.add(Dense(50))
+    dense4 = Dense(10, activation='softmax')(concat1)
+    #activ6 = Activation('softmax')(dense4)
 
-model.compile(loss='sparse_categorical_crossentropy',
-metrics=['accuracy', 'mae', 'mse'], optimizer='sgd')
+    model = Model(inputs = [input1, input2], outputs = dense4)
 
-optimizers.SGD(lr=0.003, momentum=0.002)
+    model.compile(loss='sparse_categorical_crossentropy',
+    metrics=['accuracy', 'mae', 'mse'], optimizer='sgd')
 
-history = model.fit(X, Y, batch_size= 30, epochs = 30, validation_split=0.3)
+    optimizers.SGD(lr=0.003, momentum=0.002)
+
+    history = model.fit([X, other_inp], Y, batch_size= 20, epochs = 30, validation_split=0.2)
+
+    return (history, model)
+
+history, model = create_model(X, Y, other_inp)
 
 mse = history.history['mean_squared_error']
 mae = history.history['mean_absolute_error']
-acc = history.history['val_acc']
+acc = history.history['acc']
 
 print(f'''
-    {mse}
-    {mae}
-    {acc}''')
+    mse: {max(mse)}
+    mae: {max(mae)}
+    acc: {max(acc)}''')
 
 print(model.summary())
-
-
-
